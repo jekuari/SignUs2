38b1c8c290ba758c4484923f307498941892cea9
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-08-01 00:55:27 -0600

    Add comments

--- src/App.java
+++ src/App.java
@@ -1,49 +1,54 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
-import java.util.Random;
-
 import javax.swing.*;
 
+/**
+ * This class is the main class of the application.
+ * It is used to start the application.
+ */
 public class App {
-
    private static final char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
    private static char letter = letters[(int) (Math.random() * letters.length)];
    private static int score = 0;
+
+   /* These are the components of the application */
    private static GameWindow gameWindow;
    private static Title title = new Title("SignUs!");
    private static LetterToGuess letterToGuess = new LetterToGuess(letter);
    private static ScoreLabel scoreLabel = new ScoreLabel(score);
 
+   /* This array contains all the possible answers */
    private static CardOptions[] allPossibleAnswers = {
-         new CardOptions("images/a.jpeg", 'A'),
-         new CardOptions("images/b.jpeg", 'B'),
-         new CardOptions("images/c.jpeg", 'C'),
-         new CardOptions("images/d.jpeg", 'D'),
-         new CardOptions("images/e.jpeg", 'E'),
-         new CardOptions("images/f.jpeg", 'F'),
-         new CardOptions("images/g.jpeg", 'G'),
-         new CardOptions("images/h.jpeg", 'H'),
-         new CardOptions("images/i.jpeg", 'I'),
-         new CardOptions("images/j.jpeg", 'J'),
-         new CardOptions("images/k.jpeg", 'K'),
-         new CardOptions("images/l.jpeg", 'L'),
-         new CardOptions("images/m.jpeg", 'M'),
-         new CardOptions("images/n.jpeg", 'N'),
-         new CardOptions("images/o.jpeg", 'O'),
-         new CardOptions("images/p.jpeg", 'P'),
-         new CardOptions("images/q.jpeg", 'Q'),
-         new CardOptions("images/r.jpeg", 'R'),
-         new CardOptions("images/s.jpeg", 'S'),
-         new CardOptions("images/t.jpeg", 'T'),
-         new CardOptions("images/u.jpeg", 'U'),
-         new CardOptions("images/v.jpeg", 'V'),
-         new CardOptions("images/w.jpeg", 'W'),
-         new CardOptions("images/x.jpeg", 'X'),
-         new CardOptions("images/y.jpeg", 'Y'),
-         new CardOptions("images/z.jpeg", 'Z'),
+      new CardOptions("images/a.jpeg", 'A'),
+      new CardOptions("images/b.jpeg", 'B'),
+      new CardOptions("images/c.jpeg", 'C'),
+      new CardOptions("images/d.jpeg", 'D'),
+      new CardOptions("images/e.jpeg", 'E'),
+      new CardOptions("images/f.jpeg", 'F'),
+      new CardOptions("images/g.jpeg", 'G'),
+      new CardOptions("images/h.jpeg", 'H'),
+      new CardOptions("images/i.jpeg", 'I'),
+      new CardOptions("images/j.jpeg", 'J'),
+      new CardOptions("images/k.jpeg", 'K'),
+      new CardOptions("images/l.jpeg", 'L'),
+      new CardOptions("images/m.jpeg", 'M'),
+      new CardOptions("images/n.jpeg", 'N'),
+      new CardOptions("images/o.jpeg", 'O'),
+      new CardOptions("images/p.jpeg", 'P'),
+      new CardOptions("images/q.jpeg", 'Q'),
+      new CardOptions("images/r.jpeg", 'R'),
+      new CardOptions("images/s.jpeg", 'S'),
+      new CardOptions("images/t.jpeg", 'T'),
+      new CardOptions("images/u.jpeg", 'U'),
+      new CardOptions("images/v.jpeg", 'V'),
+      new CardOptions("images/w.jpeg", 'W'),
+      new CardOptions("images/x.jpeg", 'X'),
+      new CardOptions("images/y.jpeg", 'Y'),
+      new CardOptions("images/z.jpeg", 'Z'),
    };
 
+   /* These are the buttons that display the images */
    private static ImageButton button1 = new ImageButton(allPossibleAnswers[0].imagePath, allPossibleAnswers[0].letter,
          0);
    private static ImageButton button2 = new ImageButton(allPossibleAnswers[1].imagePath, allPossibleAnswers[1].letter,
@@ -53,28 +58,33 @@ public class App {
    private static ImageButton button4 = new ImageButton(allPossibleAnswers[3].imagePath, allPossibleAnswers[3].letter,
          3);
 
+   /**
+    * This is the main method of the application.
+    * It is used to start the application.
+   */
    public static void main(String[] args) throws Exception {
       SwingUtilities.invokeLater(() -> createAndShowGUI());
    }
 
    public static void createAndShowGUI() {
       gameWindow = new GameWindow();
-      gameWindow.setVisible(true);
+      gameWindow.setVisible(true); // show the window
 
+      /* Add the components to the window */
       CountDown countdownLabel = new CountDown(30);
       gameWindow.add(countdownLabel, countdownLabel.constraints);
 
+      /* Create a timer that counts down from 30 seconds */
       Timer timer = new Timer(1000, new ActionListener() {
          int remainingTime = 30;
 
+         /* This method is called every second */
          @Override
          public void actionPerformed(ActionEvent e) {
             remainingTime--;
             if (remainingTime >= 0) {
                countdownLabel.setSecondsLeft(remainingTime);
-
             } else {
-
                ((Timer) e.getSource()).stop();
                JOptionPane.showMessageDialog(gameWindow, "Time's up! Final score: " + score);
                System.exit(0);
@@ -92,6 +102,8 @@ public class App {
       gameWindow.add(button2, button2.getConstraints());
       gameWindow.add(button3, button3.getConstraints());
       gameWindow.add(button4, button4.getConstraints());
+
+      /* Add action listeners to the buttons */
       button1.addActionListener(new ActionListener() {
          @Override
          public void actionPerformed(ActionEvent e) {
@@ -100,7 +112,6 @@ public class App {
                updateScore();
             } else {
                button1.showIncorrect();
-
             }
          }
       });
@@ -112,7 +123,6 @@ public class App {
                updateScore();
             } else {
                button2.showIncorrect();
-
             }
          }
       });
@@ -124,7 +134,6 @@ public class App {
                updateScore();
             } else {
                button3.showIncorrect();
-
             }
          }
       });
@@ -136,27 +145,27 @@ public class App {
                updateScore();
             } else {
                button4.showIncorrect();
-
             }
          }
       });
 
       timer.start();
-      startNewRount();
+      startNewRound();
    }
 
-   public static void startNewRount() {
-
+   public static void startNewRound() {
+      /* Generate a random letter */
       letter = generateRandomLetter();
       letterToGuess.setText(Character.toString(letter));
 
+      /* Generate a random set of answers */
       CardOptions[] randomCardOptions = generateRandomSetOfCardOptions(letter);
-      updateAnswers(randomCardOptions);
+      updateAnswers(randomCardOptions); // Update the answers
    }
 
    public static void updateScore() {
       scoreLabel.setText("Score:" + Integer.toString(score));
-      startNewRount();
+      startNewRound();
    }
 
    public static void updateAnswers(CardOptions[] randomCardOptions) {
