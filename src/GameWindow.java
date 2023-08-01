eefa685323a12b10c7222d0305ab0668b2c2a464
Jonathan Dominguez <jonathan24.dp@gmail.com>
2023-08-01 00:19:22 -0600

    Add comments

--- src/GameWindow.java
+++ src/GameWindow.java
@@ -1,18 +1,22 @@
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
+/**
+ * This class is a JFrame that displays the game window.
+ * It is used to display the game window.
+*/
 public class GameWindow extends JFrame {
-
    public Timer localTimer;
    public int timeLeft = 15;
 
+   /**
+    * Constructor
+   */
    public GameWindow() {
-
-      super("SignUs!");
+      super("SignUs!"); // Set the title of the window
       setLayout(new GridBagLayout());
       GridBagConstraints constraints = new GridBagConstraints();
       constraints.fill = GridBagConstraints.BOTH;
