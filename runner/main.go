7c3ea019fe7fed0d1b1be1001958cdbada28d79e
Ricardo F <ricardo@jekuari.mx>
2023-08-03 21:53:40 -0600

    added running script

---
+++ runner/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os/exec"
+)
+
+func main() {
+
+	// run java -cp /home/rfere/Documents/Utexas/Java/signUs2/SignUs2/bin App
+
+	executor := exec.Command("/usr/bin/env", "java", "-cp", "../bin", "App").Run()
+
+	if executor != nil {
+		panic(executor)
+	}
+}
