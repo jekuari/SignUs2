92b5de451b73dcec4771745592bb34f486c6775d
Ricardo F <ricardo@jekuari.mx>
2023-08-03 21:58:58 -0600

    updated exe

--- runner/main.go
+++ runner/main.go
@@ -8,7 +8,7 @@ func main() {
 
 	// run java -cp /home/rfere/Documents/Utexas/Java/signUs2/SignUs2/bin App
 
-	executor := exec.Command("/usr/bin/env", "java", "-cp", "../bin", "App").Run()
+	executor := exec.Command("java", "-cp", "../bin", "App").Run()
 
 	if executor != nil {
 		panic(executor)
